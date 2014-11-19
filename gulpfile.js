2d1e688f04e47b28e16d4022516547446996583a
Justin Johnson <booleangate@gmail.com>
2014-11-19 13:33:16 -0800

    Removed intermiedate directory src/K/.  Added ignores

--- gulpfile.js
+++ gulpfile.js
@@ -15,27 +15,6 @@ gulp.task("lint", function() {
 	return getJsStream()
 		.pipe(jshint(config.jshint))
 		.pipe(jshint.reporter("default"));
-}); 
-
-gulp.task('watch', function() {
-	var bundler = watchify(browserify('./src/index.js', watchify.args));
-
-	// Optionally, you can apply transforms
-	// and other configuration options on the
-	// bundler just as you would with browserify
-	bundler.transform('brfs');
-
-	bundler.on('update', rebundle);
-
-	function rebundle() {
-		return bundler.bundle()
-			// log errors if they happen
-			.on('error', gutil.log.bind(gutil, 'Browserify Error'))
-			.pipe(source('bundle.js'))
-			.pipe(gulp.dest('./dist'));
-	}
-
-	return rebundle();
 });
 
 gulp.task("default", ["lint"]);

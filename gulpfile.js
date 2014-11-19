4e6223c4372eec8175aa85b9938ffbfd261ffc02
Justin Johnson <booleangate@gmail.com>
2014-11-19 13:49:44 -0800

    Added 'build' and 'finalize' tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 var gulp = require("gulp");
-var config	= require('./package');
+var config	= require("./package");
 var jshint = require("gulp-jshint");
+var browserify = require("browserify");
+var rename = require("gulp-rename");
+var util = require("gulp-util");
 var source = require("vinyl-source-stream");
-var gutil = require('gulp-util');
-var browserify = require('browserify');
-
-function getJsStream() {
-	// Do not include Backbone, Bootstrap, Underscore, etc.
-	return gulp.src("./src/**/*.js");
-}
+var uglify = require("gulp-uglify");
 
 gulp.task("lint", function() {
-	return getJsStream()
+	return gulp.src("./src/**/*.js")
 		.pipe(jshint(config.jshint))
 		.pipe(jshint.reporter("default"));
 });
 
-gulp.task("default", ["lint"]);
+gulp.task("build", function() {
+	return browserify("./src/K.js")
+		.on("error", util.log.bind(util, "Browserify Error"))
+		.bundle()
+		.pipe(source("bundle.js"))
+		.pipe(rename("K.js"))
+		.pipe(gulp.dest("./dist"));
+});
+
+gulp.task("finalize", function() {
+	return gulp.src("./dist/K.js")
+		.pipe(gulp.dest("./dist"))
+		.pipe(rename("K.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("./dist"));
+});
+
+gulp.task("default", ["lint", "build", "finalize"]);
